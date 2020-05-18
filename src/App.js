256e6213c3a2a3aaa034be0e2b40033c3c5524d3
Vanessa <vnessabaker@gmail.com>
2020-05-18 10:22:47 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-const axios = require('axios')
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Latori Miller", avatar_url: "https://avatars0.githubusercontent.com/u/16719162?v=4", company: "Road to Hire"},
-  {name: "Dylan Trimble", avatar_url: "https://avatars1.githubusercontent.com/u/42077977?v=4", company: "Road to Hire"},
-];
+const axios = require('axios');
 class Form extends React.Component {
   userNameInput = React.createRef();
   handleSubmit = async (event) => {
@@ -36,39 +31,11 @@ class Form extends React.Component {
 }
 const CardList = (props) => (
   <div>
-    {props.profiles.map(profile => <Card key={profile.id} />)}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
   </div>
 )
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      profiles: []
-    }
-  }
-  addProfileData = (profileData) => {
-    this.setState(prevState => ({profiles: [...prevState.profiles, profileData]}));
-    console.log('App', profileData);
-  }
-  render() {
-    return (
-      <div>
-        <div className="header">{this.props.title}</div>
-        <Form onSubmit={this.addNewProfileData}/>
-        <CardList profiles={this.state.profiles} />
-      </div>
-    )
-  }
-}
-class ConditionalStyle extends React.Component {
-  render() {
-    return (
-      <div style = {{ color: Math.random() < 0.5 ? 'green' : 'red' }}>Look at me. I change colors</div>
-    )
-  }
-}
 class Card extends React.Component {
-  render() {
+  render(){
     const profile = this.props;
     return (
       <div className="github-profile">
@@ -78,27 +45,29 @@ class Card extends React.Component {
           <div className="company">{profile.company}</div>
         </div>
       </div>
+    );
+  }
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        profiles: []
+      };
+  }  
+  addNewProfileData = (profileData) => {
+    this.setState(prevState => ({profiles: [...prevState.profiles, profileData],
+    }));
+    console.log('App', profileData);
+  }
+  render(){
+    return (
+    <div>
+      <div className="header">{this.props.title}</div>
+      <Form onSubmit={this.addNewProfileData} />
+      <CardList profiles={this.state.profiles} />
+    </div>
     )
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
